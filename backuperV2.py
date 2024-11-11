18711f0655dae53afbdd55313fe80e3d54637de3
amadeok <43666702+amadeok@users.noreply.github.com>
2024-11-11 18:13:00 +0100

    a

--- backuperV2.py
+++ backuperV2.py
@@ -20,11 +20,12 @@ print(final_path)
 dc = {0: 'not copying', 1: 'newer file', 2: 'prev file non existent', 3: 'user setting'}
 
 copy_unchanged_files = False
-#root = "C:\\Users\\amade\\Documents\\Unreal Projects\\toBackup"
-# root = "C:\\Users\\amade\\Documents\\Unreal Projects\\VRExpPluginExample-4.26-Locked\\"
-# copy_from = "C:\\Users\\amade\\Documents\\Unreal Projects\\VRExpPluginExample-4.26-Locked\\"
-#root = "C:\\Users\\amade\\Documents\\Unreal Projects\\VRExpPluginExample-master4.27\\"
-root_copy_from = ["C:\\Users\\amade\\Documents\\Unreal Projects\\VRExpPluginExample-master4.27\\"]
+username = os.getenv("username")
+#root = f"C:\\Users\\{username}\\Documents\\Unreal Projects\\toBackup"
+# root = f"C:\\Users\\{username}\\Documents\\Unreal Projects\\VRExpPluginExample-4.26-Locked\\"
+# copy_from = f"C:\\Users\\{username}\\Documents\\Unreal Projects\\VRExpPluginExample-4.26-Locked\\"
+#root = f"C:\\Users\\{username}\\Documents\\Unreal Projects\\VRExpPluginExample-master4.27\\"
+root_copy_from = [f"C:\\Users\\{username}\\Documents\\Unreal Projects\\VRExpPluginExample-master4.27\\"]
 copy_from_flds = ['Source', "Content", "Config", "blender"]
 #copy_from = [copy_from[0] + fld for fld in copy_from_flds]
 
@@ -32,23 +33,31 @@ copy_from_flds = ['Source', "Content", "Config", "blender"]
 backup_to = ["D:\\soft\\unreal_backup_cpp_4.27\\", "E:\\soft\\unreal_backup_cpp_4.27\\"  ]
 date_based__folders = ["Content\\StylizedAssets"]
 
-root_copy_from = ["C:\\Users\\amade\\Documents\\dawd\\"]
-backup_to= ["F:\\all\\music\\backup"]
-copy_from_flds =[name for name in os.listdir(root_copy_from[0]) if os.path.isdir(root_copy_from[0]+name)]+["."]
+root_copy_from = [f"C:\\Users\\{username}\\Documents\\Studio One\\Songs\\"]
+backup_to= ["K:\\all\\music\\backup"]
+copy_from_flds =["."]#[name for name in os.listdir(root_copy_from[0]) if os.path.isdir(root_copy_from[0]+name)]+["."]
 folder_nb = 0
 folders_to_exclude = ["package\\", "Saved\\", "CachedAssetRegistry", "NativizedBuild", "v16\\", "\\Development", "\\Engine", "\\AdvancedSessions", "\\Intermediate", "\\DebugGame", "\\Binaries", "\\StylizedAssets", "\\DerivedDataCache"]#"LongswordAnimsetPro", "MedievalInfantry", "ExampleContent_CelShader"]
 nb_backup_files = 0
 
 year = 2022; month = 4; day = 1
 copy_if_recent = ["fixed", datetime(year, month, day, 0, 0)]
+extensions = [".song", ".mscz", ".ass", ".css", ".csv", ".txt", ".mid"]
 
 
 def backup_folder(root_fld_from, folder, based_on_date):
     global nb_backup_files
     for path, subdirs, files in os.walk(root_fld_from+folder):
+        
+        # for s in subdirs:
+        #     root_target = path.split(root_fld_from)[1]
+        #     td = os.path.join(target_dir, root_target, s )
+        #     os.makedirs(td, exist_ok=True)
+
         for name in files:
-            #print(os.path.join(path, name))
-            
+            if not file_has_extension(name, extensions):
+                continue
+
             root_target = path.split(root_fld_from)[1]
 
             curr_src = root_fld_from + root_target + "\\" + name
@@ -68,7 +77,7 @@ def backup_folder(root_fld_from, folder, based_on_date):
             if not based_on_date:
                 
                 if not is_excluded(target_file) and file_check != 'not copying':
-                    print("Copying: ".ljust(15), curr_src, " reason ", file_check)
+                    print(f"Copying: ({file_check:<26}) {curr_src}" )
                     copy()
                 #else:
                     #print("Not copying: ".ljust(15), curr_src)
@@ -77,12 +86,14 @@ def backup_folder(root_fld_from, folder, based_on_date):
                 new_time = os.path.getmtime(curr_src)
                 t = datetime.fromtimestamp(new_time)
                 if t > copy_if_recent[1] and file_check != 'not copying':
-                    print("Copying: ".ljust(15), curr_src, " reason ", file_check)
+                    print(f"Copying: ({file_check:<26}) {curr_src}" )
+                    # print("Copying: ".ljust(15), curr_src, " reason ", file_check)
                     copy()
 
                     
                 
-                
+def file_has_extension(filename, extensions_):
+    return filename.endswith(tuple(extensions_))
 
 def is_excluded(path):
     for excl in folders_to_exclude:
@@ -113,31 +124,33 @@ def is_missing_or_older(prev_files, new_file):
 
     return 'not copying'
 
-for copy_to in backup_to:
-    try: os.makedirs(copy_to)
-    except: pass
-    folder_nb= len(next(os.walk(copy_to))[1])
+if __name__ == "__main__":
+    for copy_to in backup_to:
+        try: os.makedirs(copy_to)
+        except: pass
+        folder_nb= len(next(os.walk(copy_to))[1])
 
-    existing_backups = [copy_to + "\\" + file for file in next(os.walk(copy_to))[1]]
+        existing_backups = [copy_to + "\\" + file for file in next(os.walk(copy_to))[1]]
 
-    now = datetime.now()
-    dt_string = now.strftime("_%d-%m-%Y_%H.%M.%S")
-    print("date and time =", dt_string)	
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d_%H.%M.%S")
+        print("date and time =", dt_string)	
 
-    target_dir = os.path.join(copy_to, str(folder_nb).zfill(4) + dt_string)
-    #prev_backup_dir = existing_backups[-1]
+        # target_dir = os.path.join(copy_to, str(folder_nb).zfill(4) + dt_string)
+        target_dir = os.path.join(copy_to,  dt_string)
+        #prev_backup_dir = existing_backups[-1]
 
-    try: os.mkdir(target_dir)
-    except: print("Error creating folder ", target_dir)
+        try: os.mkdir(target_dir)
+        except: print("Error creating folder ", target_dir)
 
-    max_size = 30
+        max_size = 30
 
-    for folder in date_based__folders:
-        backup_folder(root_copy_from[0], folder, True)
+        for folder in date_based__folders:
+            backup_folder(root_copy_from[0], folder, True)
 
-    for folder in copy_from_flds:
-        backup_folder(root_copy_from[0], folder, False)
+        for folder in copy_from_flds:
+            backup_folder(root_copy_from[0], folder, False)
 
-    if nb_backup_files == 0:
-        print("No files backuped, deleting empty dir ", target_dir)
-        os.rmdir(target_dir)
+        if nb_backup_files == 0:
+            print("No files backuped, deleting empty dir ", target_dir)
+            os.rmdir(target_dir)
